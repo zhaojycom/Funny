22a74390226df0db4fc0b49dad38e7062d3f86ae
770363647 <z770363647@126.com>
2019-09-14 15:06:07 +0800

    初始化提交

---
+++ app/src/main/java/com/zhaojy/funny/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zhaojy.funny
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
